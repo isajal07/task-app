e4d69f50d9c6fd144123308128b40ee04c3cb9af
isajal07 <isajal07@iSajal07-2.local>
2020-03-12 14:37:43 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dataset from './dataset';
+
+import HeatMap from "react-heatmap-grid";
+
+const yLabels = ["00", "", "2", "", "4", "", "6", "", "8", "", "10", "", "12", "", "14", "", "16", "", "18", "", "20", "", "22", ""];
+const xLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// const xLabels=["1","2","3","4"]
+// const yLabels=["Sun", "Mon", "Tue", "Wed"]
+
+//   const dateList = dataset.map(a=>a.date)
+//   const stringDate = new Date(dateList[10])
+
+//   console.log(stringDate.toString().split(' ')[0])
+//   const timeInList = dataset.map(a=>a.timeIn.slice(0,2))
+//   const timeOutList = dataset.map(a=>a.timeOut.slice(0,2))
+//   const time = timeInList.concat(timeOutList)
+
+
+
+//   var uniqs = time.reduce((acc, val) => {
+//     acc[val] = acc[val] === undefined ? 1 : acc[val] += 1;
+//     return acc;
+//   }, {});
+//   console.log(uniqs)
+
+// const number = () => Math.random() * 4;
+
+// const data = new Array(yLabels.length)
+//   .fill(1)
+//   .map(() =>
+//     new Array(xLabels.length).fill(1).map(() => Math.floor(number())));
+// const data = [[4,2,4,2],[2,4,2,4],[3,1,3,1],[1,3,1,3]];
+
+
+//Static data from the dataset
+const data = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 3, 1, 1, 1, 1],
+  [1, 1, 2, 1, 3, 1, 1],
+  [1, 1, 2, 2, 2, 1, 1],
+  [1, 1, 2, 2, 2, 1, 1],
+  [1, 1, 1, 2, 2, 1, 1],
+  [1, 1, 2, 2, 2, 1, 1],
+  [1, 1, 2, 1, 2, 1, 1],
+  [1, 1, 2, 2, 2, 1, 1],
+  [1, 1, 1, 1, 2, 1, 1],
+  [1, 1, 2, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1]
+]
+const index = [[1, 2, 3, 4]];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container segment">
+      Time
+      <HeatMap
+        xLabels={xLabels}
+        yLabels={yLabels}
+        xLabelsLocation={"bottom"}
+        data={data}
+
+        cellStyle={(background, value, min, max, data, x, y) => ({
+          background: `rgb(0, 151, 230, ${1 - (max - value) / (3.5 - min)})`,
+          borderStyle: 'solid',
+          borderWidth: '0.4pt',
+          borderColor: 'grey'
+
+        })}
+      />
+      <br /> <br />
+      <HeatMap
+        xLabels={["", "", "", ""]}
+        yLabels={[""]}
+        xLabelsLocation={"bottom"}
+        data={index}
+        squares
+        cellStyle={(background, value, min, max, data, x, y) => ({
+          background: `rgb(0, 151, 230, ${1 - (max - value) / 7})`,
+          margin: '0.5px',
+          borderStyle: 'solid',
+          borderWidth: '0.1px',
+          borderColor: 'grey',
+
+        })}
+      />
+
     </div>
   );
+
 }
+
 
 export default App;
---
+++ src/dataset.js
@@ -0,0 +1,37 @@
+const dataset = [
+  { date: "2020-03-10", timeIn: "11:00", timeOut: "11:10" },
+  { date: "2020-03-10", timeIn: "11:15", timeOut: "11:30" },
+  { date: "2020-03-10", timeIn: "11:31", timeOut: "11:34" },
+  { date: "2020-03-10", timeIn: "11:40", timeOut: "11:55" },
+  { date: "2020-03-10", timeIn: "12:04", timeOut: "12:50" },
+  { date: "2020-03-10", timeIn: "13:30", timeOut: "14:44" },
+  { date: "2020-03-10", timeIn: "14:55", timeOut: "16:10" },
+  { date: "2020-03-10", timeIn: "16:30", timeOut: "17:30" },
+  { date: "2020-03-10", timeIn: "17:35", timeOut: "18:35" },
+  { date: "2020-03-10", timeIn: "18:40", timeOut: "20:11" },
+
+  { date: "2020-03-11", timeIn: "09:11", timeOut: "13:13" },
+  { date: "2020-03-11", timeIn: "14:05", timeOut: "14:06" },
+  { date: "2020-03-11", timeIn: "14:30", timeOut: "15:30" },
+  { date: "2020-03-11", timeIn: "16:11", timeOut: "16:14" },
+  { date: "2020-03-11", timeIn: "16:22", timeOut: "16:33" },
+  { date: "2020-03-11", timeIn: "16:44", timeOut: "18:30" },
+  { date: "2020-03-11", timeIn: "18:33", timeOut: "18:59" },
+
+  { date: "2020-03-12", timeIn: "12:11", timeOut: "12:12" },
+  { date: "2020-03-12", timeIn: "12:13", timeOut: "12:14" },
+  { date: "2020-03-12", timeIn: "12:15", timeOut: "12:16" },
+  { date: "2020-03-12", timeIn: "12:17", timeOut: "12:30" },
+  { date: "2020-03-12", timeIn: "13:01", timeOut: "13:30" },
+  { date: "2020-03-12", timeIn: "13:31", timeOut: "13:45" },
+  { date: "2020-03-12", timeIn: "13:55", timeOut: "14:01" },
+  { date: "2020-03-12", timeIn: "14:02", timeOut: "14:40" },
+  { date: "2020-03-12", timeIn: "15:10", timeOut: "15:55" },
+  { date: "2020-03-12", timeIn: "16:13", timeOut: "17:00" },
+  { date: "2020-03-12", timeIn: "18:00", timeOut: "19:00" },
+  { date: "2020-03-12", timeIn: "19:01", timeOut: "19:02" },
+]
+
+
+
+export default dataset;
